e5bf06d678df03aaa3eff29b952befc356450e28
dzc0d3r <m4dcod3r@protonmail.com>
2023-08-13 00:17:31 +0100

    finished assignment :tada:

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+
+const main = document.getElementById("main")
+main.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+
+newHeader.textContent = "Walid is the champion"
